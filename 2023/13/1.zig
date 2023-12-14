7b2d31d3d07c1c313d3f708656ce531bf223607d
Jacob Young <jacobly0@users.noreply.github.com>
2023-12-13 23:58:20 -0500

    Zig 2023 Day 13

---
+++ 2023/13/1.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const a = gpa.allocator();
+    _ = &a;
+
+    const input = if (false)
+        \\#.##..##.
+        \\..#.##.#.
+        \\##......#
+        \\##......#
+        \\..#.##.#.
+        \\..##..##.
+        \\#.#.##.#.
+        \\
+        \\#...##..#
+        \\#....#..#
+        \\..##..###
+        \\#####.##.
+        \\#####.##.
+        \\..##..###
+        \\#....#..#
+        \\
+    else
+        @embedFile("input");
+
+    var map = std.ArrayList(u8).init(a);
+    defer map.deinit();
+
+    var line_it = std.mem.splitScalar(u8, input, '\n');
+    var summary: u64 = 0;
+    while (true) {
+        var width: u64 = undefined;
+        var height: u64 = 0;
+
+        map.clearRetainingCapacity();
+        while (line_it.next()) |line| {
+            if (line.len == 0) break;
+            width = line.len;
+            height += 1;
+            try map.appendSlice(line);
+        }
+        if (height == 0) break;
+
+        for (1..width) |x| {
+            reflection: for (0..@min(x, width - x)) |xo| {
+                for (0..height) |y| {
+                    if (map.items[(x - 1 - xo) + width * y] != map.items[(x + xo) + width * y]) {
+                        break :reflection;
+                    }
+                }
+            } else summary += x * 1;
+        }
+        for (1..height) |y| {
+            reflection: for (0..@min(y, height - y)) |yo| {
+                for (0..width) |x| {
+                    if (map.items[x + width * (y - 1 - yo)] != map.items[x + width * (y + yo)]) {
+                        break :reflection;
+                    }
+                }
+            } else summary += y * 100;
+        }
+    }
+    std.debug.print("{}\n", .{summary});
+}
