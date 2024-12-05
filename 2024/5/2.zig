4f0d5d349cdd561ad392ade7ca461d285b431bd5
Jacob Young <jacobly0@users.noreply.github.com>
2024-12-05 00:47:27 -0500

    Zig 2024 Day 5

---
+++ 2024/5/2.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const a = gpa.allocator();
+    _ = .{ stdout, a };
+
+    const input = if (false)
+        \\47|53
+        \\97|13
+        \\97|61
+        \\97|47
+        \\75|29
+        \\61|13
+        \\75|53
+        \\29|13
+        \\97|29
+        \\53|29
+        \\61|53
+        \\97|53
+        \\61|29
+        \\47|13
+        \\75|47
+        \\97|75
+        \\47|61
+        \\75|61
+        \\47|29
+        \\75|13
+        \\53|13
+        \\
+        \\75,47,61,53,29
+        \\97,61,53,29,13
+        \\75,29,13
+        \\75,97,47,61,53
+        \\61,13,29
+        \\97,13,75,29,47
+        \\
+    else
+        @embedFile("input");
+    var sum: u64 = 0;
+    var map: std.AutoHashMapUnmanaged(struct { u32, u32 }, void) = .{};
+    defer map.deinit(a);
+    var line_it = std.mem.splitScalar(u8, input, '\n');
+    while (line_it.next()) |line| {
+        if (line.len == 0) break;
+        var tok_it = std.mem.splitScalar(u8, line, '|');
+        const lhs = try std.fmt.parseInt(u32, tok_it.next().?, 10);
+        const rhs = try std.fmt.parseInt(u32, tok_it.next().?, 10);
+        try map.put(a, .{ lhs, rhs }, {});
+    }
+    var update: std.ArrayListUnmanaged(u32) = .{};
+    defer update.deinit(a);
+    while (line_it.next()) |line| {
+        if (line.len == 0) break;
+        update.clearRetainingCapacity();
+        var tok_it = std.mem.splitScalar(u8, line, ',');
+        while (tok_it.next()) |tok| try update.append(a, try std.fmt.parseInt(u32, tok, 10));
+        if (std.sort.isSorted(u32, update.items, &map, lessThan)) continue;
+        std.mem.sortUnstable(u32, update.items, &map, lessThan);
+        sum += update.items[update.items.len / 2];
+    }
+    try stdout.print("{d}\n", .{sum});
+}
+fn lessThan(map: *const std.AutoHashMapUnmanaged(struct { u32, u32 }, void), lhs: u32, rhs: u32) bool {
+    return map.contains(.{ lhs, rhs });
+}
